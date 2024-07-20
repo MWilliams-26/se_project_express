a64c15d175b486846abef5009558ba87849f7080
Michael Williams <mow26@icloud.com>
2024-07-19 23:48:19 -0400

    merging

--- .history/controllers/users_20240719234621.js
+++ .history/controllers/users_20240719234621.js
@@ -9,10 +9,10 @@ const {
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((users) => res.send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(INTERNAL_SERVER_ERROR).send({ message:  "An error has occurred on the server" });
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
     });
 };
 
@@ -24,9 +24,15 @@ const createUser = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === 'ValidationError') {
+<<<<<<< HEAD
         return res.status(BAD_REQUEST_ERROR).send({ message: "Wrong ID Fool!" });
       }
       return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
+=======
+        return res.status(BAD_REQUEST_ERROR).send({ message: "Invalid data" });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+>>>>>>> af05de2eb49308f17f00657385e4d9006d9c9577
     });
 };
 
@@ -34,16 +40,23 @@ const getUser = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
   .orFail()
-  .then((user) => res.status(200).send(user))
+  .then((user) => res.send(user))
   .catch((err) => {
       console.error(err);
       if (err.name === 'CastError') {
         return res.status(BAD_REQUEST_ERROR).send({ message: "Invalid data" });
       }
-      if (err.name === 'ValidationError') {
-        return res.status(BAD_REQUEST_ERROR).send({ message: err.message });
+<<<<<<< HEAD
+      if (err.name === 'DocumentNotFound') {
+        return res.status(BAD_REQUEST_ERROR).send({ message: "Invalid data" });
       }
-      return res.status(NOT_FOUND_ERROR).send({ message: err.message });
+      return res.status(NOT_FOUND_ERROR).send({ message: "err.message" });
+=======
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(NOT_FOUND_ERROR).send({ message: "That does not live here!" });
+      }
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server." });
+>>>>>>> af05de2eb49308f17f00657385e4d9006d9c9577
     });
 };
 
