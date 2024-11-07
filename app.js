db975ad1c4153b7353e7e51907d96d28a638df5d
Michael <mow26@icloud.com>
2024-11-07 08:56:09 -0500

    update local backend/config/gitignore

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const mainRouter = require("./routes/index");
 const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/errorHandler");
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-const JWT_SECRET = 'a gift is not enough';
+const { JWT_SECRET = 'a gift is not enough' } = process.env;
 
-module.exports = { JWT_SECRET };
+module.exports = { JWT_SECRET, };
