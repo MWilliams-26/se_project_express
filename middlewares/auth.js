6f1150f7aca4e0a3ea366d3b6483a16e8a72c4dd
Michael <mow26@icloud.com>
2024-11-13 21:36:53 -0500

    updates

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,25 @@
 const jwt = require("jsonwebtoken");
-const UnauthorizedError = require("../utils/errors/UnauthorizedError");
 const { JWT_SECRET } = require("../utils/config");
+const UnauthorizedError = require("../utils/errors/UnauthorizedError");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return next(new UnauthorizedError("User is not logged in"));
+    throw new UnauthorizedError("User isn't logged in");
   }
+
   const token = authorization.replace("Bearer ", "");
+
   let payload;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    console.error(err);
-    return next(new UnauthorizedError("Invalid token"));
+    throw new UnauthorizedError("Invalid token")
   }
 
   req.user = payload;
-
   return next();
 };
 
